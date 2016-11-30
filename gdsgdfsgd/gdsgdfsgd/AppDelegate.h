4312155b0a82063ef48db467211eb16de95c344b
常明会 <keyboardartist@163.c0m>
2016-11-30 21:05:09 +0800

    test

---
+++ gdsgdfsgd/gdsgdfsgd/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  gdsgdfsgd
+//
+//  Created by mac on 2016/11/30.
+//  Copyright © 2016年 常明会. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
